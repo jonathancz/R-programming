099a02266be38227ea0b4f4f810d5a06f6d3b96c
Jonathan Chen <jonathanchenzheng@hotmail.com>
2018-10-04 18:01:22 -0400

    Create simple R program

---
+++ RTut.R
@@ -0,0 +1,19 @@
+mlbPlayers = read.table(file=file.choose(),
+                        header=T, sep=" ",
+                        na.strings="`",
+                        stringsAsFactors=F)
+
+# Grab just RBIs and Avg for each player
+# playerData is known as a data frame (Table of Data)
+# We get the stats we want by passing that list in a vector
+playerData = mlbPlayers[,c("RBI","AVG")]
+
+# Create the file
+png(file="player_rbi_avg.png")
+
+# Create the plot
+plot(x=playerData$RBI, y=playerData$AVG,
+     xlab="RBI", ylab="AVG", main="RBIs and Average")
+
+# Create the file
+dev.off()
