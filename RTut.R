3ad2217f079508ddc931cb440b2f44c689e48cc8
Jonathan Chen <jonathanchenzheng@hotmail.com>
2018-10-20 23:12:45 -0400

    Include, Factors, Data Frames, and Looping

--- RTut.R
+++ RTut.R
@@ -203,3 +203,100 @@ strVect = strsplit("A dog ran fast", " ")
  
 strVect
 
+# ----- FACTORS ------
+# Factors are used when you have a limited number of values
+# that are strings or integers
+ 
+# Create a factor vector
+direction = c("Up", "Down", "Left", "Right", "Left", "Up")
+factorDir = factor(direction)
+ 
+# Check if it's a Factor
+is.factor(factorDir)
+ 
+# A Factor object contains levels which store all possible
+# values
+levels(x=factorDir)
+ 
+# You can define your levels and their orders
+dow = c("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+ 
+wDays = c("Tuesday", "Thursday", "Monday")
+ 
+wdFact = factor(x=wDays, levels=dow, ordered=T)
+ 
+wdFact
+ 
+# ----- DATA FRAMES -----
+# A Data Frame is a table which contains any type 
+# of data and an equal amount of data in each column
+ 
+# Each row is called a record and each column a varaible
+ 
+# Create customer data frame
+custData = data.frame(name=c("Tom", "Sally", "Sue"),
+                      age=c(43, 28, 35), 
+                      stringsAsFactors=F)
+ 
+custData
+ 
+# Get data in row 1 column 1
+custData[1,1]
+ 
+# Get all data in 1st row
+custData[1,1:2]
+ 
+# Get all ages
+custData[1:3, 2]
+ 
+# Get dimensions
+dim(custData)
+ 
+# Add another record
+recordMark = data.frame(name="Mark", age=33)
+custData = rbind(custData, recordMark)
+custData
+ 
+# Add a column representing debt
+debt = c(0, 25.50, 36, 48.19)
+custData = cbind(custData, debt)
+custData
+ 
+# Check if money is owed
+owesMoney = custData[custData$debt > 0,]
+owesMoney
+ 
+# ----- LOOPING -----
+# Repeat until a condition is met
+num = 1
+repeat{
+  print(num)
+  num = num + 1
+  if(num > 5){
+    
+    # Jumps out of loop
+    break
+  }
+}
+ 
+# Repeat while condition is true
+while(num > 0){
+  num = num - 1
+  
+  # next skips the rest of the loop and jumps
+  # back to the top
+  if(num %% 2 == 0){
+    next
+  }
+  print(num)
+}
+ 
+# For can be used to cycle through a vector
+# or do the same thing a specific number of times
+oneTo5 = 1:5
+for (i in oneTo5){
+  print(i)
+}
+
+
